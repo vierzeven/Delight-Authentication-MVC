2c2ed6dda821c6d7f7bb526e303a3266b6446a1c
Joey Schmitz <4plus7@gmail.com>
2020-10-07 23:17:56 +0200

    Enforce unique usernames and exclude non-printing control characters and certain printable special characters

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -11,11 +11,13 @@ class AuthController
     public function processregistration($auth)
     {
         try {
-            $userId = $auth->register($_POST['email'], $_POST['password'], $_POST['username'], function ($selector, $token) {
-                echo 'Send ' . $selector . ' and ' . $token . ' to the user (e.g. via email)';
-            });
-
-            echo 'We have signed up a new user with the ID ' . $userId;
+            $username = $_POST['username'];
+            if (preg_match('/[\x00-\x1f\x7f\/:\\\\]/', $username) === 0) {
+                $userId = $auth->registerWithUniqueUsername($_POST['email'], $_POST['password'], $username, function ($selector, $token) {
+                    echo 'Send ' . $selector . ' and ' . $token . ' to the user (e.g. via email)';
+                });
+                echo 'We have signed up a new user with the ID ' . $userId;
+            }
         } catch (InvalidEmailException $e) {
             die('Invalid email address');
         } catch (InvalidPasswordException $e) {
@@ -24,6 +26,8 @@ class AuthController
             die('User already exists');
         } catch (TooManyRequestsException $e) {
             die('Too many requests');
+        } catch (DuplicateUsernameException $e) {
+            die('Username already exists');
         }
     }
 
